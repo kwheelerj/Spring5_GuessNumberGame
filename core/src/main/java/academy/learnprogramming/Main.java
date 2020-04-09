1c632099666a5ca9a6eeaa65164ff3276606ad6e
kwheelerj <kwheelerj@gmail.com>
2020-04-09 12:52:18 -0400

    init commit and core submodule added

---
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,38 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    private static final String CONFIG_LOCATION = "beans.xml";
+
+
+    public static void main(String[] args) {
+        log.info("Guess the number game");
+
+        // create context (container) first
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        // get the number generator bean from the context (container)
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        // call method to get a random number
+        int number = numberGenerator.next();
+
+        log.info("number = {}", number);
+
+        // get the game bean from the context (container)
+        Game game = context.getBean("game", Game.class);
+
+        // call reset method
+        game.reset();
+
+        // close context (container); prevent memory resource leaks
+        context.close();
+    }
+}
