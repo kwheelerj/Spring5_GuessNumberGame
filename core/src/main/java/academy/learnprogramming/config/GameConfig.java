40e0b5bfdffddace7f9ead1b27609237c8a9e316
kwheelerj <kwheelerj@gmail.com>
2020-04-16 17:43:24 -0400

    added properties file for constants: PropertySource and Value annotations used

--- core/src/main/java/academy/learnprogramming/config/GameConfig.java
+++ core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -2,14 +2,20 @@ package academy.learnprogramming.config;
 
 import academy.learnprogramming.GuessCount;
 import academy.learnprogramming.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
-
-        private int maxNumber = 100;
-        private int guessCount = 10;
+//        private int maxNumber = 100;
+//        private int guessCount = 10;
+    @Value("${game.maxNumber}")
+    private int maxNumber;
+    @Value("${game.guessCount}")
+    private int guessCount;
 
         @Bean
         @MaxNumber
