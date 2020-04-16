8162a89162e8e2d7589b23ecbb4186ccabfe2ef0
kwheelerj <kwheelerj@gmail.com>
2020-04-16 15:31:46 -0400

    Annotation EventListener instead

--- console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
+++ console/src/main/java/academy/learnprogramming/console/ConsoleNumberGuess.java
@@ -4,15 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+//public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class ConsoleNumberGuess {
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        // ContextRefreshedEvent is published when the ApplicationContext is init or refreshed.
-        log.info("Container ready for use.");
+//    @Override
+//    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+    // ContextRefreshedEvent is published when the ApplicationContext is init or refreshed.
+    @EventListener
+    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+        log.info("start -> Container ready for use.");
     }
 }
