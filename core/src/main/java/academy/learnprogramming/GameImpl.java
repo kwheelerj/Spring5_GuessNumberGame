32d2743bfdb3f54a36191400d106299de1aa83d3
kwheelerj <kwheelerj@gmail.com>
2020-04-17 03:47:28 -0400

    Lombok added to GameImpl, ConsoleNumberGuess, NumberGeneratorImpl, MessageGeneratorImpl

--- core/src/main/java/academy/learnprogramming/GameImpl.java
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -1,21 +1,26 @@
 package academy.learnprogramming;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 
+@Slf4j
+@Getter
 @Component
 public class GameImpl implements Game {
-    private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
+    @Getter(AccessLevel.NONE)
     private final NumberGenerator numberGeneratorField;   // A dependency for GameIpl bean
     private final int guessCount;
 
     private int number;
+    @Setter
     private int guess;
     private int smallest;
     private int largest;
@@ -46,41 +51,6 @@ public class GameImpl implements Game {
     }
 
     @Override
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public int getGuess() {
-        return guess;
-    }
-
-    @Override
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-
-    @Override
-    public int getSmallest() {
-        return smallest;
-    }
-
-    @Override
-    public int getLargest() {
-        return largest;
-    }
-
-    @Override
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-
-    @Override
-    public int getGuessCount() {
-        return guessCount;
-    }
-
-    @Override
     public void check() {
         checkValidNumberRange();
         if (validNumberRange) {
@@ -92,11 +62,6 @@ public class GameImpl implements Game {
             }
             remainingGuesses--;
         }
-    }
-
-    @Override
-    public boolean isValidNumberRange() {
-        return validNumberRange;
     }
 
     @Override
--- core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -1,13 +1,17 @@
 package academy.learnprogramming;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.Random;
 
 
+@Getter
 @Component
 public class NumberGeneratorImpl implements NumberGenerator {
 
+    @Getter(AccessLevel.NONE)
     private final Random random = new Random();
     private final int maxNumber;
     private final int minNumber;
@@ -21,15 +25,5 @@ public class NumberGeneratorImpl implements NumberGenerator {
     @Override
     public int next() {
         return random.nextInt(maxNumber);
-    }
-
-    @Override
-    public int getMaxNumber() {
-        return maxNumber;
-    }
-
-    @Override
-    public int getMinNumber() {
-        return minNumber;
     }
 }
