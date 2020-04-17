4383faa588fdc84062bacfa1164485a3a7a67ad8
kwheelerj <kwheelerj@gmail.com>
2020-04-17 01:40:25 -0400

    lombok added to project, @Slf4j on Main

--- console/src/main/java/academy/learnprogramming/console/Main.java
+++ console/src/main/java/academy/learnprogramming/console/Main.java
@@ -3,12 +3,13 @@ package academy.learnprogramming.console;
 import academy.learnprogramming.config.GameConfig;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 public class Main {
-
     public static void main(String[] args) {
-
+        log.info("app started");
         ConfigurableApplicationContext context =
                 new AnnotationConfigApplicationContext(GameConfig.class);
 
